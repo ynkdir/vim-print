54705188a5c8ec9fe225758cd6fe46b1e358cb8d
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2011-02-27 20:57:55 +0900

    no comment

---
+++ autoload/print/syntax.vim
@@ -0,0 +1,210 @@
+
+function! print#syntax#dump(outfile, ...)
+  let mode = get(a:000, 0, {})
+  let s:syncache = {}
+  let s:.mode = mode
+  let out = s:dump()
+  call writefile(out, a:outfile)
+endfunction
+
+let s:syncache = {}
+let s:mode = {}
+
+function! s:dump()
+  let out = []
+
+  call add(out, s:paper(595, 842))
+  call add(out, s:margin(25, 25, 25, 25))
+  call add(out, s:header(expand('%:t') . '%=Page %N', 1))
+  call add(out, s:number(6))
+  call add(out, s:linespace(2))
+  call add(out, s:font('Courier', 10))
+  call add(out, s:start())
+
+  let curattr = {}
+
+  for lnum in range(1, line('$'))
+    call add(out, s:line())
+    let synline = s:synline(lnum)
+    let textbuf = []
+    for [str, attr] in synline
+      if attr != curattr
+        if !empty(textbuf)
+          call add(out, s:text(join(textbuf, '')))
+          let textbuf = []
+        endif
+        let curattr = attr
+        call add(out, s:highlight(attr))
+      endif
+      call add(textbuf, str)
+    endfor
+    if !empty(textbuf)
+      call add(out, s:text(join(textbuf, '')))
+    endif
+  endfor
+
+  call add(out, s:end())
+
+  return out
+endfunction
+
+function! s:paper(width, height)
+  return printf('PAPER %f %f', a:width, a:height)
+endfunction
+
+function! s:margin(left, top, right, bottom)
+  return printf('MARGIN %f %f %f %f', a:left, a:top, a:right, a:bottom)
+endfunction
+
+function! s:header(format, extraline)
+  return printf('HEADER %s %d', s:string(a:format), a:extraline)
+endfunction
+
+function! s:number(numberwidth)
+  return printf('NUMBER %d', a:numberwidth)
+endfunction
+
+function! s:linespace(height)
+  return printf('LINESPACE %f', a:height)
+endfunction
+
+function! s:font(name, size)
+  return printf('FONT %s %d', s:string(a:name), a:size)
+endfunction
+
+function! s:text(s)
+  return printf('TEXT %s', s:string(a:s))
+endfunction
+
+function! s:line()
+  return 'LINE'
+endfunction
+
+function! s:highlight(attr)
+  return printf("HIGHLIGHT %s #%02x%02x%02x #%02x%02x%02x #%02x%02x%02x %d %d %d %d",
+        \ s:string(a:attr.name),
+        \ a:attr.fg[0], a:attr.fg[1], a:attr.fg[2],
+        \ a:attr.bg[0], a:attr.bg[1], a:attr.bg[2],
+        \ a:attr.sp[0], a:attr.sp[1], a:attr.sp[2],
+        \ a:attr.bold,
+        \ a:attr.italic,
+        \ a:attr.underline,
+        \ a:attr.undercurl)
+endfunction
+
+function! s:start()
+  return "START"
+endfunction
+
+function! s:end()
+  return "END"
+endfunction
+
+function! s:string(s)
+  return '"' . escape(a:s, '"\') . '"'
+endfunction
+
+function! s:hi()
+  redir => buf
+  silent hi
+  redir END
+  let attrs = {}
+  for line in split(buf, '\n')
+    let m = matchlist(line, '^\(\h\w*)')
+    if !empty(m)
+      let attr = s:synattr(hlID(m[1]))
+      let attrs[attr.name] = attr
+    endif
+  endfor
+  return attrs
+endfunction
+
+function! s:synline(lnum)
+  let res = []
+  let col = 1
+  let vcol = 0
+  for c in split(getline(a:lnum), '\zs')
+    let vw = strdisplaywidth(c, vcol)
+    if c == "\t"
+      let attr = s:synattr(synID(a:lnum, col, 1))
+      call add(res, [repeat(' ', vw), attr])
+    elseif c =~ '[[:cntrl:]]'
+      let attr = s:synattr(hlID('SpecialKey'))
+      call add(res, [strtrans(c), attr])
+    elseif c !~ '[[:print:]]'
+      let attr = s:synattr(hlID('SpecialKey'))
+      call add(res, [strtrans(c), attr])
+    else
+      let attr = s:synattr(synID(a:lnum, col, 1))
+      call add(res, [c, attr])
+    endif
+    let col += len(c)
+    let vcol += vw
+  endfor
+  return res
+endfunction
+
+function! s:synattr(id)
+  if has_key(s:syncache, a:id)
+    return s:syncache[a:id]
+  endif
+  let normal_fg = s:syncolor(hlID('Normal'), 'fg#', [0, 0, 0])
+  let normal_bg = s:syncolor(hlID('Normal'), 'bg#', [255, 255, 255])
+  let attr = {}
+  let attr.name = s:synidattr(a:id, 'name')
+  let attr.fg = s:syncolor(a:id, 'fg#', normal_fg)
+  let attr.bg = s:syncolor(a:id, 'bg#', normal_bg)
+  let attr.sp = s:syncolor(a:id, 'sp#', attr.fg)
+  let attr.bold = s:synidattr(a:id, 'bold')
+  let attr.italic = s:synidattr(a:id, 'italic')
+  let attr.reverse = s:synidattr(a:id, 'reverse')
+  let attr.inverse = s:synidattr(a:id, 'inverse')
+  let attr.standout = s:synidattr(a:id, 'standout')
+  let attr.underline = s:synidattr(a:id, 'underline')
+  let attr.undercurl = s:synidattr(a:id, 'undercurl')
+  let s:syncache[a:id] = attr
+  return attr
+endfunction
+
+function! s:syncolor(id, what, default)
+  let c = s:synidattr(a:id, a:what)
+  if c =~ '^\d'
+    let c = s:cterm_color[c]
+  endif
+  if c != '-1' && c != ''
+    return [str2nr(c[1:2], 16), str2nr(c[3:4], 16), str2nr(c[5:6], 16)]
+  endif
+  return a:default
+endfunction
+
+function! s:synidattr(id, what)
+  " Normal is not to be translated.
+  if a:id == 0 || a:id == hlID('Normal')
+    let id = hlID('Normal')
+  else
+    let id = synIDtrans(a:id)
+  endif
+  " Normal's attribute does not effect.
+  if id == hlID('Normal') && a:what !~ '\v^%(name|font|fg#?|bg#?|sp#?)$'
+    return 0
+  endif
+  if empty(s:mode)
+    return synIDattr(id, a:what)
+  else
+    return synIDattr(id, a:what, s:mode)
+  endif
+endfunction
+
+" copied from runtime/syntax/2html.vim
+let s:cterm_color = {0: "#000000", 1: "#c00000", 2: "#008000", 3: "#804000", 4: "#0000c0", 5: "#c000c0", 6: "#008080", 7: "#c0c0c0", 8: "#808080", 9: "#ff6060", 10: "#00ff00", 11: "#ffff00", 12: "#8080ff", 13: "#ff40ff", 14: "#00ffff", 15: "#ffffff"}
+
+" Colors for 88 and 256 come from xterm.
+call extend(s:cterm_color, {16: "#000000", 17: "#00008b", 18: "#0000cd", 19: "#0000ff", 20: "#008b00", 21: "#008b8b", 22: "#008bcd", 23: "#008bff", 24: "#00cd00", 25: "#00cd8b", 26: "#00cdcd", 27: "#00cdff", 28: "#00ff00", 29: "#00ff8b", 30: "#00ffcd", 31: "#00ffff", 32: "#8b0000", 33: "#8b008b", 34: "#8b00cd", 35: "#8b00ff", 36: "#8b8b00", 37: "#8b8b8b", 38: "#8b8bcd", 39: "#8b8bff", 40: "#8bcd00", 41: "#8bcd8b", 42: "#8bcdcd", 43: "#8bcdff", 44: "#8bff00", 45: "#8bff8b", 46: "#8bffcd", 47: "#8bffff", 48: "#cd0000", 49: "#cd008b", 50: "#cd00cd", 51: "#cd00ff", 52: "#cd8b00", 53: "#cd8b8b", 54: "#cd8bcd", 55: "#cd8bff", 56: "#cdcd00", 57: "#cdcd8b", 58: "#cdcdcd", 59: "#cdcdff", 60: "#cdff00", 61: "#cdff8b", 62: "#cdffcd", 63: "#cdffff", 64: "#ff0000"})
+call extend(s:cterm_color, {65: "#ff008b", 66: "#ff00cd", 67: "#ff00ff", 68: "#ff8b00", 69: "#ff8b8b", 70: "#ff8bcd", 71: "#ff8bff", 72: "#ffcd00", 73: "#ffcd8b", 74: "#ffcdcd", 75: "#ffcdff", 76: "#ffff00", 77: "#ffff8b", 78: "#ffffcd", 79: "#ffffff", 80: "#2e2e2e", 81: "#5c5c5c", 82: "#737373", 83: "#8b8b8b", 84: "#a2a2a2", 85: "#b9b9b9", 86: "#d0d0d0", 87: "#e7e7e7"})
+
+call extend(s:cterm_color, {16: "#000000", 17: "#00005f", 18: "#000087", 19: "#0000af", 20: "#0000d7", 21: "#0000ff", 22: "#005f00", 23: "#005f5f", 24: "#005f87", 25: "#005faf", 26: "#005fd7", 27: "#005fff", 28: "#008700", 29: "#00875f", 30: "#008787", 31: "#0087af", 32: "#0087d7", 33: "#0087ff", 34: "#00af00", 35: "#00af5f", 36: "#00af87", 37: "#00afaf", 38: "#00afd7", 39: "#00afff", 40: "#00d700", 41: "#00d75f", 42: "#00d787", 43: "#00d7af", 44: "#00d7d7", 45: "#00d7ff", 46: "#00ff00", 47: "#00ff5f", 48: "#00ff87", 49: "#00ffaf", 50: "#00ffd7", 51: "#00ffff", 52: "#5f0000", 53: "#5f005f", 54: "#5f0087", 55: "#5f00af", 56: "#5f00d7", 57: "#5f00ff", 58: "#5f5f00", 59: "#5f5f5f", 60: "#5f5f87", 61: "#5f5faf", 62: "#5f5fd7", 63: "#5f5fff", 64: "#5f8700"})
+call extend(s:cterm_color, {65: "#5f875f", 66: "#5f8787", 67: "#5f87af", 68: "#5f87d7", 69: "#5f87ff", 70: "#5faf00", 71: "#5faf5f", 72: "#5faf87", 73: "#5fafaf", 74: "#5fafd7", 75: "#5fafff", 76: "#5fd700", 77: "#5fd75f", 78: "#5fd787", 79: "#5fd7af", 80: "#5fd7d7", 81: "#5fd7ff", 82: "#5fff00", 83: "#5fff5f", 84: "#5fff87", 85: "#5fffaf", 86: "#5fffd7", 87: "#5fffff", 88: "#870000", 89: "#87005f", 90: "#870087", 91: "#8700af", 92: "#8700d7", 93: "#8700ff", 94: "#875f00", 95: "#875f5f", 96: "#875f87", 97: "#875faf", 98: "#875fd7", 99: "#875fff", 100: "#878700", 101: "#87875f", 102: "#878787", 103: "#8787af", 104: "#8787d7", 105: "#8787ff", 106: "#87af00", 107: "#87af5f", 108: "#87af87", 109: "#87afaf", 110: "#87afd7", 111: "#87afff", 112: "#87d700"})
+call extend(s:cterm_color, {113: "#87d75f", 114: "#87d787", 115: "#87d7af", 116: "#87d7d7", 117: "#87d7ff", 118: "#87ff00", 119: "#87ff5f", 120: "#87ff87", 121: "#87ffaf", 122: "#87ffd7", 123: "#87ffff", 124: "#af0000", 125: "#af005f", 126: "#af0087", 127: "#af00af", 128: "#af00d7", 129: "#af00ff", 130: "#af5f00", 131: "#af5f5f", 132: "#af5f87", 133: "#af5faf", 134: "#af5fd7", 135: "#af5fff", 136: "#af8700", 137: "#af875f", 138: "#af8787", 139: "#af87af", 140: "#af87d7", 141: "#af87ff", 142: "#afaf00", 143: "#afaf5f", 144: "#afaf87", 145: "#afafaf", 146: "#afafd7", 147: "#afafff", 148: "#afd700", 149: "#afd75f", 150: "#afd787", 151: "#afd7af", 152: "#afd7d7", 153: "#afd7ff", 154: "#afff00", 155: "#afff5f", 156: "#afff87", 157: "#afffaf", 158: "#afffd7"})
+call extend(s:cterm_color, {159: "#afffff", 160: "#d70000", 161: "#d7005f", 162: "#d70087", 163: "#d700af", 164: "#d700d7", 165: "#d700ff", 166: "#d75f00", 167: "#d75f5f", 168: "#d75f87", 169: "#d75faf", 170: "#d75fd7", 171: "#d75fff", 172: "#d78700", 173: "#d7875f", 174: "#d78787", 175: "#d787af", 176: "#d787d7", 177: "#d787ff", 178: "#d7af00", 179: "#d7af5f", 180: "#d7af87", 181: "#d7afaf", 182: "#d7afd7", 183: "#d7afff", 184: "#d7d700", 185: "#d7d75f", 186: "#d7d787", 187: "#d7d7af", 188: "#d7d7d7", 189: "#d7d7ff", 190: "#d7ff00", 191: "#d7ff5f", 192: "#d7ff87", 193: "#d7ffaf", 194: "#d7ffd7", 195: "#d7ffff", 196: "#ff0000", 197: "#ff005f", 198: "#ff0087", 199: "#ff00af", 200: "#ff00d7", 201: "#ff00ff", 202: "#ff5f00", 203: "#ff5f5f", 204: "#ff5f87"})
+call extend(s:cterm_color, {205: "#ff5faf", 206: "#ff5fd7", 207: "#ff5fff", 208: "#ff8700", 209: "#ff875f", 210: "#ff8787", 211: "#ff87af", 212: "#ff87d7", 213: "#ff87ff", 214: "#ffaf00", 215: "#ffaf5f", 216: "#ffaf87", 217: "#ffafaf", 218: "#ffafd7", 219: "#ffafff", 220: "#ffd700", 221: "#ffd75f", 222: "#ffd787", 223: "#ffd7af", 224: "#ffd7d7", 225: "#ffd7ff", 226: "#ffff00", 227: "#ffff5f", 228: "#ffff87", 229: "#ffffaf", 230: "#ffffd7", 231: "#ffffff", 232: "#080808", 233: "#121212", 234: "#1c1c1c", 235: "#262626", 236: "#303030", 237: "#3a3a3a", 238: "#444444", 239: "#4e4e4e", 240: "#585858", 241: "#626262", 242: "#6c6c6c", 243: "#767676", 244: "#808080", 245: "#8a8a8a", 246: "#949494", 247: "#9e9e9e", 248: "#a8a8a8", 249: "#b2b2b2", 250: "#bcbcbc", 251: "#c6c6c6", 252: "#d0d0d0", 253: "#dadada", 254: "#e4e4e4", 255: "#eeeeee"})
+
---
+++ backend/cairo/print.c
@@ -0,0 +1,717 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include <cairo.h>
+#include <cairo-ps.h>
+#include <cairo-pdf.h>
+#include <cairo-ft.h>
+
+
+/* FIXME: don't use constant */
+#define LINENR_MARGIN 10
+
+struct Options {
+    double paper_width;
+    double paper_height;
+    double margin_left;
+    double margin_top;
+    double margin_right;
+    double margin_bottom;
+    char *header_format;
+    int header_extraline;
+    int number_width;
+    double linespace;
+    char *font_name;
+    double font_size;
+};
+
+
+struct Color {
+    double r;
+    double g;
+    double b;
+};
+
+
+struct Highlight {
+    char *name;
+    struct Color fg;
+    struct Color bg;
+    struct Color sp;
+    int bold;
+    int italic;
+    int underline;
+    int undercurl;
+};
+
+
+struct PrintContext {
+    int pagenum;
+    int linenum;
+    double font_height;
+    double font_descent;
+    double numberwidth;
+    double y;
+    double x;
+    struct Highlight hi;
+};
+
+
+static int endswith(const char *haystack, const char *needle);
+static int utf8len(const char *str);
+static void error(const char *message, ...);
+static void skip_space();
+static int read_char();
+static char *read_command();
+static char *read_string();
+static int read_integer();
+static double read_float();
+static struct Color read_color();
+static void command_paper();
+static void command_margin();
+static void command_header();
+static void command_number();
+static void command_linespace();
+static void command_font();
+static void command_highlight();
+static void command_text();
+static void command_line();
+static void command_start();
+static void command_end();
+static int is_white(struct Color color);
+static void set_font(const char *name, double size, int bold, int italic);
+static void newline();
+static void newpage();
+static void print_number();
+static void print_header();
+static void print_text(const char *text, struct Highlight hi);
+static void print();
+
+
+static char *infile;
+static char *outfile;
+static FILE *in;
+static struct Options options;
+static struct PrintContext pc;
+static cairo_surface_t *surface;
+static cairo_t *cr;
+
+
+static int
+endswith(const char *haystack, const char *needle)
+{
+    const char *a;
+    const char *b;
+
+    a = haystack + strlen(haystack) - 1;
+    b = needle + strlen(needle) - 1;
+    while (haystack <= a && needle <= b && *a == *b) {
+        a--;
+        b--;
+    }
+    return (b < needle);
+}
+
+
+static int
+utf8len(const char *str)
+{
+    unsigned char c;
+
+    c = (unsigned char)str[0];
+    if ((c & 0x80) == 0) {
+        return 1;
+    } else if ((c & 0xE0) == 0xC0) {
+        return 2;
+    } else if ((c & 0xF0) == 0xE0) {
+        return 3;
+    } else if ((c & 0xF8) == 0xF0) {
+        return 4;
+    } else if ((c & 0xFC) == 0xF8) {
+        return 5;
+    } else if ((c & 0xFE) == 0xFC) {
+        return 6;
+    }
+
+    error("invalid utf8");
+}
+
+
+static void
+error(const char *format, ...)
+{
+    va_list ap;
+
+    va_start(ap, format);
+    vfprintf(stderr, format, ap);
+    fprintf(stderr, "\n");
+    va_end(ap);
+
+    exit(EXIT_FAILURE);
+}
+
+
+static void
+skip_space()
+{
+    fscanf(in, "%*[ \t\r\n]");
+}
+
+
+static int
+read_char()
+{
+    int c;
+
+    c = fgetc(in);
+    if (c == EOF) {
+        error("unexpected EOF");
+    }
+
+    return c;
+}
+
+
+static char *
+read_command()
+{
+    char buf[256];
+    int n;
+
+    n = fscanf(in, "%s", (char *)&buf);
+    if (n != 1) {
+        error("read_command error");
+    }
+
+    return strdup(buf);
+}
+
+
+static char *
+read_string()
+{
+    int c;
+    char *buf = NULL;
+    size_t bufsize = 0;
+
+    skip_space();
+    c = read_char();
+    if (c != '"') {
+        error("unexpected character: %d", c);
+    }
+
+    for (;;) {
+        bufsize += 1;
+        buf = realloc(buf, bufsize);
+        c = read_char();
+        if (c == '"') {
+            buf[bufsize - 1] = '\0';
+            break;
+        }
+        if (c == '\\') {
+            c = read_char();
+        }
+        buf[bufsize - 1] = c;
+    }
+
+    return buf;
+}
+
+
+static int
+read_integer()
+{
+    int n;
+    int x;
+
+    n = fscanf(in, "%d", &x);
+    if (n != 1) {
+        error("read_integer error");
+    }
+
+    return x;
+}
+
+
+static double
+read_float()
+{
+    int n;
+    double x;
+
+    n = fscanf(in, "%lf", &x);
+    if (n != 1) {
+        error("read_float error");
+    }
+
+    return x;
+}
+
+
+static struct Color
+read_color()
+{
+    int n;
+    int r, g, b;
+    struct Color color;
+
+    skip_space();
+    n = fscanf(in, "#%2x%2x%2x", &r, &g, &b);
+    if (n != 3) {
+        error("read_color error");
+    }
+
+    color.r = r / 255.0;
+    color.g = g / 255.0;
+    color.b = b / 255.0;
+
+    return color;
+}
+
+
+static void
+command_paper()
+{
+    options.paper_width = read_float();
+    options.paper_height = read_float();
+}
+
+
+static void
+command_margin()
+{
+    options.margin_left = read_float();
+    options.margin_top = read_float();
+    options.margin_right = read_float();
+    options.margin_bottom = read_float();
+}
+
+
+static void
+command_header()
+{
+    options.header_format = read_string();
+    options.header_extraline = read_integer();
+}
+
+
+static void
+command_number()
+{
+    options.number_width = read_integer();
+}
+
+
+static void
+command_linespace()
+{
+    options.linespace = read_float();
+}
+
+
+static void
+command_font()
+{
+    options.font_name = read_string();
+    options.font_size = read_float();
+}
+
+static void
+command_highlight()
+{
+    struct Highlight hi;
+
+    hi.name = read_string();
+    hi.fg = read_color();
+    hi.bg = read_color();
+    hi.sp = read_color();
+    hi.bold = read_integer();
+    hi.italic = read_integer();
+    hi.underline = read_integer();
+    hi.undercurl = read_integer();
+
+    if (pc.hi.name != NULL) {
+        free(pc.hi.name);
+    }
+    pc.hi = hi;
+}
+
+
+static void
+command_text()
+{
+    char *text;
+
+    text = read_string();
+    print_text(text, pc.hi);
+    free(text);
+}
+
+
+static void
+command_line()
+{
+    newline();
+}
+
+
+static void
+command_start()
+{
+    cairo_font_extents_t fe;
+    cairo_text_extents_t te;
+
+    if (endswith(outfile, ".ps")) {
+        surface = cairo_ps_surface_create(outfile,
+                options.paper_width, options.paper_height);
+    } else if (endswith(outfile, ".pdf")) {
+        surface = cairo_pdf_surface_create(outfile,
+                options.paper_width, options.paper_height);
+    } else {
+        error("file type is not supported: %s", outfile);
+    }
+
+    cr = cairo_create(surface);
+
+    pc.pagenum = 0;
+    pc.linenum = 0;
+
+    set_font(options.font_name, options.font_size, 0, 0);
+
+    /* FIXME: How to get line height and baseline offset?
+     * Use linespace option for workaround. */
+    cairo_font_extents(cr, &fe);
+    pc.font_height = fe.height + options.linespace;
+    pc.font_descent = fe.descent + options.linespace / 2;
+
+    if (options.number_width > 0) {
+        /* FIXME: What is correct way? */
+        cairo_text_extents(cr, "0", &te);
+        pc.numberwidth = options.number_width * te.x_advance + LINENR_MARGIN;
+    } else {
+        pc.numberwidth = 0;
+    }
+}
+
+
+static void
+command_end()
+{
+    cairo_show_page(cr);
+
+    if (cr != NULL) {
+        cairo_destroy(cr);
+        cr = NULL;
+    }
+
+    if (surface != NULL) {
+        cairo_surface_finish(surface);
+        cairo_surface_destroy(surface);
+        surface = NULL;
+    }
+}
+
+
+static int
+is_white(struct Color color)
+{
+    return (color.r == 1 && color.g == 1 && color.b == 1);
+}
+
+
+static void
+set_font(const char *name, double size, int bold, int italic)
+{
+    cairo_font_slant_t slant;
+    cairo_font_weight_t weight;
+
+    if (italic) {
+        slant = CAIRO_FONT_SLANT_ITALIC;
+    } else {
+        slant = CAIRO_FONT_SLANT_NORMAL;
+    }
+
+    if (bold) {
+        weight = CAIRO_FONT_WEIGHT_BOLD;
+    } else {
+        weight = CAIRO_FONT_WEIGHT_NORMAL;
+    }
+
+    if (endswith(name, ".ttf")) {
+#if CAIRO_HAS_FT_FONT
+        /* FIXME: bold? italic? */
+        static char prev_name[512] = {0};
+        static FT_Library library = NULL;
+        FT_Face face;
+        FT_Error err;
+        cairo_font_face_t *f;
+        int face_index = 0;
+        int load_flags = 0;
+
+        if (strcmp(prev_name, name) != 0) {
+            if (library != NULL) {
+                err = FT_Init_FreeType(&library);
+                if (err) {
+                    error("FT_Init_FreeType failed");
+                }
+            }
+
+            err = FT_New_Face(library, name, face_index, &face);
+            if (err) {
+                error("FT_New_Face failed");
+            }
+
+            f = cairo_ft_font_face_create_for_ft_face(face, load_flags);
+
+            cairo_set_font_face(cr, f);
+        }
+
+        cairo_set_font_size(cr, size);
+#else
+        error("ttf is not supported");
+#endif
+    } else {
+        /* FIXME: How to embed? */
+        cairo_select_font_face(cr, name, slant, weight);
+        cairo_set_font_size(cr, size);
+    }
+}
+
+
+static void
+newline()
+{
+    if (pc.linenum == 0) {
+        newpage();
+    } else {
+        pc.y += pc.font_height;
+        if (pc.y + pc.font_height > options.paper_height - options.margin_bottom) {
+            newpage();
+        }
+    }
+
+    pc.linenum += 1;
+
+    print_number();
+
+    pc.x = options.margin_left + pc.numberwidth;
+}
+
+
+static void
+newpage()
+{
+    if (pc.pagenum != 0) {
+        cairo_show_page(cr);
+    }
+
+    pc.pagenum += 1;
+
+    print_header();
+
+    pc.x = options.margin_left + pc.numberwidth;
+    pc.y = options.margin_top + pc.font_height * (1 + options.header_extraline);
+}
+
+
+static void
+print_number()
+{
+    char fmt[256];
+    char buf[256];
+    cairo_text_extents_t te;
+    struct Highlight hi;
+
+    if (options.number_width <= 0) {
+        return;
+    }
+
+    sprintf(fmt, "%%%dd", options.number_width);
+    sprintf(buf, fmt, pc.linenum);
+
+    /* FIXME: load from file */
+    hi.name = "LineNr";
+    hi.fg.r = 0;
+    hi.fg.g = 0;
+    hi.fg.b = 0;
+    hi.bg.r = 1;
+    hi.bg.g = 1;
+    hi.bg.b = 1;
+    hi.sp = hi.fg;
+    hi.bold = 0;
+    hi.italic = 0;
+    hi.underline = 0;
+    hi.undercurl = 0;
+
+    cairo_text_extents(cr, buf, &te);
+    pc.x = options.margin_left + pc.numberwidth - LINENR_MARGIN - te.x_advance;
+    print_text(buf, hi);
+}
+
+
+static void
+print_header()
+{
+    char left[1024];
+    char right[1024];
+    char *out;
+    char *p;
+    cairo_text_extents_t te;
+    struct Highlight hi;
+
+    if (options.header_format == NULL || options.header_format[0] == '\0') {
+        return;
+    }
+
+    left[0] = '\0';
+    right[0] = '\0';
+
+    out = left;
+
+    for (p = options.header_format; *p != '\0'; ++p) {
+        if (*p == '%') {
+            ++p;
+            if (*p == '%') {
+                *out++ = '%';
+            } else if (*p == 'N') {
+                out += sprintf(out, "%d", pc.pagenum);
+            } else if (*p == '=') {
+                out = right;
+            } else {
+                error("unknown header item: %c", *p);
+            }
+        } else {
+            *out++ = *p;
+        }
+        *out = '\0';
+    }
+
+    /* FIXME: load from file */
+    hi.name = "PageHeader";
+    hi.fg.r = 0;
+    hi.fg.g = 0;
+    hi.fg.b = 0;
+    hi.bg.r = 1;
+    hi.bg.g = 1;
+    hi.bg.b = 1;
+    hi.sp = hi.fg;
+    hi.bold = 0;
+    hi.italic = 0;
+    hi.underline = 0;
+    hi.undercurl = 0;
+
+    pc.x = options.margin_left;
+    pc.y = options.margin_top;
+    print_text(left, hi);
+
+    cairo_text_extents(cr, right, &te);
+    pc.x = options.paper_width - options.margin_right - te.x_advance;
+    pc.y = options.margin_top;
+    print_text(right, hi);
+}
+
+
+static void
+print_text(const char *text, struct Highlight hi)
+{
+    cairo_text_extents_t te;
+    const char *p;
+    int len;
+    char buf[256];
+
+    set_font(options.font_name, options.font_size, hi.bold, hi.italic);
+
+    /* FIXME: Is it better not to write one by one? */
+    for (p = text; *p != '\0'; p += len) {
+        len = utf8len(p);
+        memmove(buf, p, len);
+        buf[len] = '\0';
+
+        cairo_text_extents(cr, buf, &te);
+
+        if (pc.x + te.x_advance > options.paper_width - options.margin_right) {
+            pc.y += pc.font_height;
+            if (pc.y + pc.font_height >
+                    options.paper_height - options.margin_bottom) {
+                newpage();
+            }
+            pc.x = options.margin_left + pc.numberwidth;
+        }
+
+        if (!is_white(hi.bg)) {
+            cairo_set_source_rgb(cr, hi.bg.r, hi.bg.g, hi.bg.b);
+            cairo_rectangle(cr, pc.x, pc.y, te.x_advance, pc.font_height);
+            cairo_fill(cr);
+        }
+
+        cairo_set_source_rgb(cr, hi.fg.r, hi.fg.g, hi.fg.b);
+        cairo_move_to(cr, pc.x, pc.y + pc.font_height - pc.font_descent);
+        cairo_show_text(cr, buf);
+
+        pc.x += te.x_advance;
+    }
+}
+
+
+static void
+print()
+{
+    char *command;
+    struct Highlight hi;
+    char *text;
+
+    for (;;) {
+        skip_space();
+        if (feof(in)) {
+            break;
+        }
+        command = read_command();
+        if (strcmp(command, "PAPER") == 0) {
+            command_paper();
+        } else if (strcmp(command, "MARGIN") == 0) {
+            command_margin();
+        } else if (strcmp(command, "HEADER") == 0) {
+            command_header();
+        } else if (strcmp(command, "NUMBER") == 0) {
+            command_number();
+        } else if (strcmp(command, "LINESPACE") == 0) {
+            command_linespace();
+        } else if (strcmp(command, "FONT") == 0) {
+            command_font();
+        } else if (strcmp(command, "HIGHLIGHT") == 0) {
+            command_highlight();
+        } else if (strcmp(command, "TEXT") == 0) {
+            command_text();
+        } else if (strcmp(command, "LINE") == 0) {
+            command_line();
+        } else if (strcmp(command, "START") == 0) {
+            command_start();
+        } else if (strcmp(command, "END") == 0) {
+            command_end();
+        } else {
+            error("unknown command: %s", command);
+        }
+        free(command);
+    }
+}
+
+
+int
+main(int argc, char **argv)
+{
+
+    infile = argv[1];
+    outfile = argv[2];
+
+    in = fopen(infile, "r");
+
+    print();
+
+    fclose(in);
+
+    return 0;
+}
+
